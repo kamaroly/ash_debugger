d5d9e4121959378464db4efff56618f7a5749d0b
Kamaro <kamaroly@gmail.com>
2025-01-03 16:53:22 +0300

    Formatted codes

--- test/ash_debugger_web/controllers/error_html_test.exs
+++ test/ash_debugger_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule AshDebuggerWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(AshDebuggerWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(AshDebuggerWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
